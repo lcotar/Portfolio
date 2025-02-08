5f0675cefc482b6acc7d6c1964b37f387b981cec
Lea Cotar <lea.cotar@gmx.ch>
2025-02-08 10:36:22 +0100

    first commit

---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-landingPage',
+  standalone: true,
+  imports: [CommonModule],
+  template: /* html */ ` <section></section> `,
+  styleUrls: [],
+})
+export class LandingPageComponent {}
