3a0d61e898d9dd6651a1acee1ba05784cde0c345
Lea Cotar <lea.cotar@gmx.ch>
2025-08-05 17:24:57 +0200

    add translation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ImprintComponent } from './imprint/imprint.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'privacy', component: PrivacyComponent },
+  { path: '**', redirectTo: '' },
+];
