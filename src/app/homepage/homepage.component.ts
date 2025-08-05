8d5732eee7c083f9c04505c787a25b5a9d04b627
Lea Cotar <lea.cotar@gmx.ch>
2025-08-05 17:38:17 +0200

    add content

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 
+import { HomeComponent } from '../home/home.component';
+import { AboutMeComponent } from '../about-me/about-me.component';
+import { MySkillsComponent } from '../my-skills/my-skills.component';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
+import { WhatMyColleaguesSayComponent } from '../what-my-colleagues-say/what-my-colleagues-say.component';
+
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [
+    HomeComponent,
+    AboutMeComponent,
+    MySkillsComponent,
+    PortfolioComponent,
+    WhatMyColleaguesSayComponent,
+  ],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.scss'
+  styleUrl: './homepage.component.scss',
 })
-export class HomepageComponent {
-
-}
+export class HomepageComponent {}
