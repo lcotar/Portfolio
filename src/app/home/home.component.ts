f1bd1547b967b90267b0d351cc9f091944c7c3a0
Lea Cotar <lea.cotar@gmx.ch>
2025-08-06 15:56:03 +0200

    navbar & home section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
