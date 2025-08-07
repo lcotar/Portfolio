3da63c31d8db288f9ca92ad62437bd24a31dbeb1
Lea Cotar <lea.cotar@gmx.ch>
2025-08-07 14:41:51 +0200

    home section

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrl: './about-me.component.scss',
 })
-export class AboutMeComponent {
-
-}
+export class AboutMeComponent {}
