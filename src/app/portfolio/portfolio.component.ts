6e20402235d9e3adf944415e29d86e5e609277ab
Lea Cotar <lea.cotar@gmx.ch>
2025-08-08 15:56:35 +0200

    Portfolio Projects

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { SingleProjectComponent } from './single-project/single-project.component';
+import { CommonModule } from '@angular/common';
+import { Project } from '../interfaces/projects.interface';
+import { TranslateModule } from '@ngx-translate/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [SingleProjectComponent, CommonModule, TranslateModule],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrl: './portfolio.component.scss',
 })
-export class PortfolioComponent {
-
-}
+export class PortfolioComponent {}
