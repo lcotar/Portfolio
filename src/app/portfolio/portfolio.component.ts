4cb0832a031eb95a2c2d4d7a6e7143ec86fc379c
Lea Cotar <lea.cotar@gmx.ch>
2025-08-09 11:30:37 +0200

    Portfolio section

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -12,4 +12,39 @@ import * as AOS from 'aos';
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss',
 })
-export class PortfolioComponent {}
+export class PortfolioComponent {
+  projects: Project[] = [
+    {
+      projectname: 'Join',
+      content: {
+        projectname: 'Join',
+        title: 'myProjects.join.title',
+        technologies: 'HTML | CSS | Javascript',
+        description: 'myProjects.join.description',
+      },
+
+      imageProject: ['/img/portfolio_projects/01_join.png'],
+      github: 'https://github.com/johannesngl/join-main',
+      livetest: 'https://join.leacotar.com/',
+    },
+    {
+      projectname: 'Pollo Loco',
+      content: {
+        title: 'myProjects.polloLoco.title',
+        technologies: 'HTML | CSS | Javascript',
+        description: 'myProjects.polloLoco.description',
+      },
+      imageProject: ['/img/portfolio_projects/02_el_pollo_loco.png'],
+      github: 'https://github.com/lcotar/El-Pollo-Loco',
+      livetest: 'https://pollo-loco.leacotar.com/',
+    },
+  ];
+
+  activeProject = this.projects[0];
+
+  // projectnavbar: string[] = ['Join', 'Pollo Loco'];
+
+  changeIndex(index: number) {
+    this.activeProject = this.projects[index];
+  }
+}
--- src/app/portfolio/single-project/single-project.component.ts
+++ src/app/portfolio/single-project/single-project.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { Project } from '../../interfaces/projects.interface';
 
 @Component({
   selector: 'app-single-project',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './single-project.component.html',
-  styleUrl: './single-project.component.scss'
+  styleUrl: './single-project.component.scss',
 })
 export class SingleProjectComponent {
-
+  @Input() project!: Project;
 }
