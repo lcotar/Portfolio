a96ea06cceac2b940094e08a38df1a38fed5c0cf
Lea Cotar <lea.cotar@gmx.ch>
2025-08-11 15:41:23 +0200

    contact Section

--- src/app/contact-me/contactform/contactform.component.ts
+++ src/app/contact-me/contactform/contactform.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-contactform',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, TranslateModule, CommonModule, RouterModule],
   templateUrl: './contactform.component.html',
-  styleUrl: './contactform.component.scss'
+  styleUrl: './contactform.component.scss',
 })
 export class ContactformComponent {
+  /* thankYouVisible = false;
+    http=inject(HttpClient) */
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
 
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
+    }
+  }
 }
