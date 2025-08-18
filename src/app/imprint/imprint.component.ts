aad14720142d3d7b37fc908683fb6c028c7dbe90
Lea Cotar <lea.cotar@gmx.ch>
2025-08-18 11:40:18 +0200

    Imprint

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,12 +1,12 @@
+import { TranslateModule } from '@ngx-translate/core';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, RouterModule],
   templateUrl: './imprint.component.html',
-  styleUrl: './imprint.component.scss'
+  styleUrl: './imprint.component.scss',
 })
-export class ImprintComponent {
-
-}
+export class ImprintComponent {}
