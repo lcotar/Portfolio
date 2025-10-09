712e5833d7752f071e711919416e2d6525387101
Lea Cotar <lea.cotar@gmx.ch>
2025-10-09 16:06:46 +0200

    changes content

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { NavbarResponsivComponent } from './navbar-responsiv/navbar-responsiv.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TranslateModule, CommonModule, RouterModule],
+  imports: [
+    TranslateModule,
+    CommonModule,
+    RouterModule,
+    NavbarResponsivComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
--- src/app/home/navbar-responsiv/navbar-responsiv.component.ts
+++ src/app/home/navbar-responsiv/navbar-responsiv.component.ts
@@ -1,12 +1,40 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar-responsiv',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, TranslateModule],
   templateUrl: './navbar-responsiv.component.html',
   styleUrl: './navbar-responsiv.component.scss',
 })
-export class NavbarResponsivComponent {}
+export class NavbarResponsivComponent {
+  currentLang: string = 'en';
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
+
+  toogleTranslation(languae: string) {
+    this.translate.use(languae);
+    const CURRENT_LANGUAGE = this.translate.currentLang;
+  }
+
+  /* isOpen: boolean = false;
+
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+  }
+
+  closeMenu() {
+    this.isOpen = false;
+  } */
+
+  menuOpen = false;
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+}
